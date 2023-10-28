b64c2d3333d09a890088c4db84056097a6bd5465
5p1ke5 <bstrazdas@gmail.com>
2023-10-27 20:29:27 -0500

    Think I'm done with player controller script. Making new build 10272023_2029

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float playerSpeed = 4.0f; //How fast the player moves.
+    private int runMod = 2; //Multiplier to movement speed while run button is held down
+    private int jumpMod = 2; //The player's gravity is divided by this while the jump button is held down.
+    public float jumpHeight = 1f;
+    public float gravityValue = -10f;
+
     private CharacterController controller;
     private Vector3 playerVelocity;
-    private bool groundedPlayer;
-    private float playerSpeed = 4.0f;
-    private float jumpHeight = 3.0f;
-    private float gravityValue = -9.81f;
+    private bool grounded;
 
     private void Start()
     {
@@ -18,24 +21,36 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        groundedPlayer = controller.isGrounded;
-        if (groundedPlayer && playerVelocity.y < 0)
+        //If the controller is grounded sets vertical speed to 0.
+        grounded = controller.isGrounded;
+        if (grounded && playerVelocity.y < 0)
         {
             playerVelocity.y = 0f;
         }
 
-        Vector3 vertical = transform.forward * Input.GetAxis("Vertical") * playerSpeed * Time.deltaTime;
-        Vector3 horizontal = transform.right * Input.GetAxis("Horizontal") * playerSpeed * Time.deltaTime;
+        //If the run button is held down multiplies movement speed by run mod.
+        int running = Input.GetButton("Fire3") ? runMod : 1;
+
+        //Gets vertical and horizontal controller axes and multiplies them by the transforms axes to get two vectors, then uses those vectors to move the object.
+        Vector3 vertical = transform.forward * Input.GetAxis("Vertical") * playerSpeed * running * Time.deltaTime;
+        Vector3 horizontal = transform.right * Input.GetAxis("Horizontal") * playerSpeed * running * Time.deltaTime;
         Vector3 vector = vertical + horizontal;
 
         controller.Move(vector);
 
-        if (Input.GetButton("Jump") && groundedPlayer)
+        //Initial button press makes the player jump if they're grounded (can add in a double jump or something later)
+        if (Input.GetButtonDown("Jump"))
         {
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+            if (grounded)
+            {
+                playerVelocity.y += Mathf.Sqrt(jumpHeight * -1f * gravityValue);
+            }
         }
 
-        playerVelocity.y += gravityValue * Time.deltaTime;
+        //Additionally if the jump button is being held down offsets gravity, allowing the player to jump higher by holding the jump button.
+        int gravOffset = Input.GetButton("Jump") ? jumpMod : 1;
+
+        playerVelocity.y += (gravityValue / gravOffset) * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
     }
 }
