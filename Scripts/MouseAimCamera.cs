1c410b96350a084a7f3f49dfe45fba271cc19513
5p1ke5 <bstrazdas@gmail.com>
2023-11-12 21:50:48 -0600

    Made it so camera zooms in and out. NP

--- Scripts/MouseAimCamera.cs
+++ Scripts/MouseAimCamera.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +7,8 @@ public class MouseAimCamera : MonoBehaviour
 {
     public GameObject target;
     public float rotationSpeed = 5;
+    public float maxZoom = -10;
+    public float minZoom = -2;
 
     Vector3 offset;
 
@@ -25,6 +28,12 @@ public class MouseAimCamera : MonoBehaviour
         float desiredAngle = target.transform.eulerAngles.y;
         Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
         transform.position = target.transform.position + (rotation * offset);
+
+        Vector2 zoom = Input.mouseScrollDelta;
+        offset.z += zoom.y;
+
+        offset.z = Math.Max(maxZoom, offset.z);
+        offset.z = Math.Min(minZoom, offset.z);
 
         transform.LookAt(target.transform);
     }
