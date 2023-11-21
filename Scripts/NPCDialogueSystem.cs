74474d44785a0e49ad24b7a7839189106554b626
Sami Stuart <samistuart@MacBook-Pro-158.local>
2023-11-21 11:43:34 -0600

    fixed the menus, so when starting the game the menu behaves correctly.

--- Scripts/NPCDialogueSystem.cs
+++ Scripts/NPCDialogueSystem.cs
@@ -2,27 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityStandardAssets.Cameras;
 
 public class NPCDialogueSystem : MonoBehaviour
 {
-    public string text = "Hello traveller!";
+    //Time to wait between displaying the texts in the array.
+    public float textTimeIncrement = 5.0f;
 
+    //Different messages for the NPC to display. One message is ideal but you can do more if necessary.
+    public string[] texts;
+
+    //Text component that will display texts. Will usually be set in prefab.
     public Text dialogueText;
+
     // Player Detection value assigned to false
     bool playerDetection = false;
+
+    //Timer variable.
+    private float textTimer;
+    private int textIndex = 0;
 
     void Start()
     {
         dialogueText.gameObject.SetActive(false);
+        textTimer = textTimeIncrement;
     }
     
     void Update()
     {
-        // If statement so if player is in the detection range and presses F on NPC
-        // Then Dialogue print statement in console is shown for now
-       if(playerDetection)
+        if (playerDetection)
        {
-            dialogueText.text = text;
+            dialogueText.text = texts[textIndex];
+
+            //Increment timer.
+            if (textTimer > 0)
+            {
+                textTimer -= Time.deltaTime;
+            }
+            //If timer finishes counting down increments index, resets timer.
+            else
+            {
+                textIndex++;
+                if (textIndex >= texts.Length)
+                {
+                    //If index reaches the end of the texts array resets it to 0.
+                    textIndex = 0;
+                }
+
+                textTimer = textTimeIncrement;
+            }
        } 
        else 
        {
@@ -39,7 +67,7 @@ public class NPCDialogueSystem : MonoBehaviour
             dialogueText.gameObject.SetActive(true);
         }
     }
-        private void OnTriggerExit(Collider other)
+    private void OnTriggerExit(Collider other)
     {
         // Player Detection is set to false when left range
             playerDetection = false;
