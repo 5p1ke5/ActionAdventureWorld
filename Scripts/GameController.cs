beddcf1fa52abdadb196f82a50e0e145a46a67c3
Sami Stuart <samistuart@MacBook-Pro-158.local>
2023-11-21 22:08:44 -0600

    fixed merge conflict w gamecontroller

--- Scripts/GameController.cs
+++ Scripts/GameController.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class GameController : MonoBehaviour
 {
     public static GameController Instance { get; private set; }
-<<<<<<< HEAD
+
     public GameObject menuTreeGameObject;
     // public static MenuBehavior menuBehavior;
     // Start is called before the first frame update
@@ -72,49 +72,9 @@ public class GameController : MonoBehaviour
 
     }
     // Update is called once per frame
-=======
-    public GameObject menuTreeGameObject;
-   // public static MenuBehavior menuBehavior;
-    // Start is called before the first frame update
-    private void Start()
-    {
-        //Debug.Log("gamecontroller timescale is: " + (Time.timeScale == 1 ? 1 : 0));
 
-        
-      
-        MenuBehavior.menuTree = menuTreeGameObject;
-        string currentScene = SceneManager.GetActiveScene().name;
 
-        switch (currentScene)
-        {
-            case "Game":
 
-                //this right here is tweaking for some reason it has to do with update i believe because if i do the exact same thing in update this works
-
-                //GameObject startMenu = MenuBehavior.ReturnMenuGameObject("StartMenu");
-                //MenuBehavior.ToggleMenu(startMenu);
-
-                Debug.Log("scene is: " + currentScene);
-                break;
-            case "CityLevel":
-                Debug.Log("scene is: " +currentScene);
-                break;
-            case "IceLevel":
-                Debug.Log("scene is: " + currentScene);
-                break;
-            case "ScoccerLevel":
-                Debug.Log("scene is: " + currentScene);
-                break;
-            case "WaterLevel":
-                Debug.Log("scene is: " + currentScene);
-                break;
-        }
-        
-
-    }
-
-    // Update is called once per frame
->>>>>>> 9b4202a75fc4f514e3b5459b4671e7c88bf6fac4
     void Update()
     {
         if (Input.GetKeyUp(KeyCode.Escape)&& Time.timeScale ==1)
